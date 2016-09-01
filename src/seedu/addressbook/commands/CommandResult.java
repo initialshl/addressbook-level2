c1dc483348c4ff72a6ee687d29a6d5a656bbdb46
initialshl <initials.hl@live.com>
2016-09-01 10:29:17 +0800

    Fix indentations

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -16,7 +16,7 @@ public class CommandResult {
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
-	public CommandResult(String feedbackToUser) {
+    public CommandResult(String feedbackToUser) {
         this.feedbackToUser = feedbackToUser;
         relevantPersons = null;
     }
@@ -25,13 +25,13 @@ public class CommandResult {
         this.feedbackToUser = feedbackToUser;
         this.relevantPersons = relevantPersons;
     }
-    
+
     /**
-	 * Returns the feedback message to be shown to the user.
-	 */
-	public String getFeedbackToUser() {
-		return feedbackToUser;
-	}
+     * Returns the feedback message to be shown to the user.
+     */
+    public String getFeedbackToUser() {
+        return feedbackToUser;
+    }
 
     /**
      * Returns list of persons relevant to the command command result, if any.
